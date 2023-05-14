48867a88eb504086ae452901ff3ad7f06d9f11c5
hychoe0 <hychoe@umich.edu>
2023-05-13 21:22:52 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Project identifier: 0E04A31E0D60C01986ACB20081C9D8722A1899B6
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // TODO: implement main function to read input, perform analysis, and output results
+  cout << "Hello World" << endl;
+  return 0;
+}
